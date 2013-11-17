edceb3b34bf53627d384145bf6d4a4bcc1f9f21b
Rich Gautier <rgautier@gmail.com>
2013-11-17 11:50:46 -0500

    First two projects in Numbers Solution

---
+++ Numbers/Fibonacci_to_N/Fibonacci_to_N.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_to_N
+{
+    class Fibonacci_to_N
+    {
+        static void Main(string[] args)
+        {
+            int digits;
+            //Determine the length of the sequence to display based on Command Line Argument or input
+            if (args.Length == 0)
+            {
+                Console.Write("How long of a Fibonacci sequence to display? ");
+
+                //Never trust a user provided input
+                try
+                {
+                    digits = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("I accept only an integer count - no decimals or strings");
+                    digits = 0;
+                }
+
+            }
+            else
+            {
+                //Never trust a user provided input
+                try
+                {
+                    digits = int.Parse(args[0]);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("I accept only an integer count - no decimals or strings");
+                    digits = 0;
+                } 
+            }
+
+            //If the sequence requested is >0, then start a loop to generate them, else print an error message
+            if (digits<1)
+            {
+                Console.WriteLine("I can only generate a positive number (more than 0) length sequence");
+            }
+            else
+            {
+                //Store the Fibonacci sequence counters
+                
+                uint fibCurrent = 0;
+                uint fibNext = 1;
+                
+                //Loop through and create the sequence and output it for us.
+                for (int i = 0; i < digits; i++)
+                {
+                    Console.Write("{0} ",fibCurrent);
+                    
+                    fibNext = fibNext + fibCurrent;
+                    fibCurrent = fibNext - fibCurrent;
+                }
+                Console.WriteLine();                    //end of line
+            }
+
+            //Wait for user input so that executing the code from Windows will keep window open.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Numbers/Pi_to_N/Pi_to_N.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi_to_N
+{
+    class Pi_to_N
+    {
+        static void Main(string[] args)
+        {
+            //Initialize variables
+            int digits;
+            string formatString = "{0:#.";
+
+            //Figure out how many digits base on first argument, or ask..
+            if (args.Length == 0)
+            {
+                Console.WriteLine("How many digits of pi to display (from 1 to max 15 for now):");
+                try
+                {
+                    digits = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You're an ass");
+                    digits = 0;
+                }
+                
+            }
+            else
+            {
+                digits = int.Parse(args[0]);
+            }
+
+            //TODO: Actually compute PI out to as many digits as asked.
+            //Math.PI only provides pi to 15 digits
+            if ((digits < 1) || (digits > 15))
+            {
+                //User did not follow directions
+                Console.WriteLine("Minimum is 1 digit, max is 15");
+            }
+            else
+            {
+                //User followed directions
+                //Set up the formatString for the output based on digit count
+                //Starting with 1 digit displayed (counting from 1)
+                for (int i = 1; i < digits; i++)
+                {
+                    formatString += "#";
+                }
+                formatString += "} is Pi to {1} digits.";
+
+                //Output Math.PI based on the input
+                Console.WriteLine(formatString, Math.PI, digits);
+            }
+            //Wait for user input so that executing the code from Windows will keep window open.
+            Console.ReadLine();
+        }
+    }
+}
