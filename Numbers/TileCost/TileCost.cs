87b233be0c873bfa7144d4de76829095e90e774f
Rich Gautier <rgautier@gmail.com>
2013-11-19 09:18:48 -0500

    Updated TileCost
    Updated TileCost to go beyond approximation and determine best fit of
    tiles to room dimensions and buy extra tiles as needed

--- Numbers/TileCost/TileCost.cs
+++ Numbers/TileCost/TileCost.cs
@@ -20,6 +20,70 @@ namespace TileCost
             per_tile = get_input_float("Enter the cost per tile: ");
 
             Console.WriteLine("The cost to tile this will be approximately: {0:C}",per_tile*((width*height)/(tile_width*tile_height)));
+
+            uint rows, cols;
+
+            //TODO: Compute whether there are going to be underruns on any of the dimensions and add to the width and/or height to compensate and recalculate
+            //First, check the width and make sure it's divisible by tile width or height evenly
+            if (width % tile_height == 0)
+            {
+                cols = width / tile_height;
+                //The tiles fit the room width to height
+                if (height % tile_width == 0)
+                {
+                    //The tiles fit the room evenly
+                    rows = height / tile_width;
+                }
+                else
+                {
+                    //The tiles width does not evenly fit the room (but the height does)
+                    rows = (height / tile_width) + 1;
+                }
+
+            }
+            else if (width % tile_width == 0)
+            {
+                cols = width / tile_width;
+                //The tiles fit the room width to width
+                if (height % tile_height == 0)
+                {
+                    //The tiles fit the room evenly
+                    rows = height / tile_height;
+                }
+                else
+                {
+                    //The tiles height does not evenly fit the room (but the width does)
+                    rows = (height / tile_height) + 1;
+                }
+            }
+            else
+            {
+                //The room width doesn't evenly fit to the tiles in either dimension so extra column
+                  //Reserve judgement on this until we know whether height fits tiles evenly
+                //The tiles fit the room width to width
+                if (height % tile_height == 0)
+                {
+                    //The tiles fit the room evenly height to height
+                    rows = height / tile_height;
+                    cols = (width / tile_width) + 1;
+                }
+                else if (height % tile_width == 0)
+                {
+                    ////The tiles fit the room evenly height to width
+                    rows = height / tile_width;
+                    cols = (width / tile_height) + 1;
+                }
+                else
+                {
+                    //We will need both an extra row and column of tiles to compensate
+                    rows = (height / tile_height) + 1;
+                    cols = (width / tile_width) + 1;
+                }
+                
+                
+                
+            }
+            Console.WriteLine("To make sure you have enough tiles ({0}) you need to spend {1:C}", rows * cols, per_tile * (rows * cols));
             Console.ReadLine();
         }
 
