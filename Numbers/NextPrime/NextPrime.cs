f8aa2a0e0d953e5c722f9353746418b2b33a4877
Rich Gautier <rgautier@gmail.com>
2013-11-18 20:57:09 -0500

    Added NextPrimes and TileCost Solutions

---
+++ Numbers/NextPrime/NextPrime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextPrime
+{
+    class NextPrime
+    {
+        static void Main(string[] args)
+        {
+            UInt64 thisprime = 1;       //0 is not prime - start with 1
+            string Entry="";
+            
+            while (Entry != "N")
+            {
+                while (is_prime(thisprime) != 1)
+                {
+                    thisprime += 1;
+                }
+                
+                Console.Write("The next prime number is {0} - Enter \"N\" to exit: ", thisprime);    
+                Entry = Console.ReadLine();
+                thisprime += 1;
+            }
+        }
+
+        //function that checks if a number is prime 
+        //returns 1 or returns smallest prime it can be divided by
+        public static UInt64 is_prime(UInt64 numToCheck)
+        {
+            //We count up toward the number to check divided by the current check number for divisors.
+            //Holy shit, that's faster!
+            for (UInt64 i = 2; i <= numToCheck / i; i++)
+            {
+                if (numToCheck % i == 0)
+                {
+                    return i;
+                }
+            }
+            //If it wasn't divisible by any of the numbers under it, it's divisible by itself and 1
+            return 1;
+        }
+    }
+}
---
+++ Numbers/TileCost/TileCost.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileCost
+{
+    class TileCost
+    {
+        static void Main(string[] args)
+        {
+            uint height, width, tile_height, tile_width;
+            float per_tile;
+
+            width = get_input_uint("Enter the Width of the floor: ");
+            height = get_input_uint("Enter the Height of the floor: ");
+            tile_width = get_input_uint("Enter the Width of the tiles: ");
+            tile_height = get_input_uint("Enter the Height of the tiles: ");
+            per_tile = get_input_float("Enter the cost per tile: ");
+
+            Console.WriteLine("The cost to tile this will be approximately: {0:C}",per_tile*((width*height)/(tile_width*tile_height)));
+            Console.ReadLine();
+        }
+
+        public static uint get_input_uint(string prompt)
+        {
+            string answer;
+            uint num_answer = 0;
+
+            Console.Write(prompt);
+            answer = Console.ReadLine();
+            bool test = true;
+
+            do
+            {
+                try
+                {
+                    num_answer = uint.Parse(answer);
+                    test = false;
+                }
+                catch (Exception)
+                {
+                    Console.Write("I need a positive integer value\n{0}", prompt);
+                    answer = Console.ReadLine();
+                }
+            } while (test);
+
+            return num_answer;
+        }
+
+        public static float get_input_float(string prompt)
+        {
+            string answer;
+            float num_answer = 0;
+
+            Console.Write(prompt);
+            answer = Console.ReadLine();
+            bool test = true;
+
+            do
+            {
+                try
+                {
+                    num_answer = float.Parse(answer);
+                    test = false;
+                }
+                catch (Exception)
+                {
+                    Console.Write("I need a positive floating point value\n{0}", prompt);
+                    answer = Console.ReadLine();
+                }
+            } while (test);
+
+            return num_answer;
+        }
+    }
+}
